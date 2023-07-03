5a438972d8ed86dcfeba0a5e51baebc295331c4c
sam <samuelayo61@gmail.com>
2023-07-03 19:04:38 +0100

    added

---
+++ server.js
@@ -0,0 +1,44 @@
+//mongodb
+//require('./config/db');
+
+//const app = require('express')();
+// const port = 3000;
+
+// const UserRouter = require('./api/User');
+
+//For accpeting post from data
+// const bodyParser = require('express').json;
+// app.use(bodyParser());
+
+// app.use('/user', UserRouter)
+
+// app.listen(port, () =>{
+    // console.log(`Server running on port ${port}`);
+// })
+
+//FFt8ckS3PzeRAmPv
+//MONGO_URI=mongodb://mongodb-myapp:27017/myapp
+//MONGODB_URI=mongodb+srv://sam:FFt8ckS3PzeRAmPv@cluster0.yqma9l4.mongodb.net/UserDB?retryWrites=true&w=majority
+//MONGODB_URI=mongodb+srv://sam:ov39KRW2BkILdGnv@cluster0.yqma9l4.mongodb.net/?retryWrites=true&w=majority
+//MONGODB_URI=mongodb+srv://sam:QJfuZc62rWJV0tzN@cluster0.yqma9l4.mongodb.net/UserDB?retryWrites=true&w=majority
+
+
+
+
+require('./config/db');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const UserRouter = require('./api/User');
+
+// For accepting POST request data
+app.use(express.json());
+
+app.use('/user', UserRouter);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
